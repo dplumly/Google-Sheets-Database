a5769268eda904bee71ca31becc4152f2d14111e
Donnie Plumly <donnie.plumly@gmail.com>
2024-11-22 09:04:26 -0800

    removing js, adding 2nd form

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
+console.log('init')
+// const delay = 1000;
 
-const delay = 1000;
+// document.getElementById('form').addEventListener('submit', (event) => {
+//     event.preventDefault();
 
-document.getElementById('form').addEventListener('submit', (event) => {
-    event.preventDefault();
+//     setTimeout(() => {
+//         document.getElementById('thank-you-modal').style.display = 'block';
+//     }, delay);
+// })
 
-    setTimeout(() => {
-        document.getElementById('thank-you-modal').style.display = 'block';
-    }, delay);
-})
-
-let thankYou = addEventListener('click', () => {
-    console.log('message sent!')
-});
+// let thankYou = addEventListener('click', () => {
+//     console.log('message sent!')
+// });
