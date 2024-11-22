432ba26c3c7664acb4b8460fd7e673d09e8d2d9e
Donnie Plumly <donnie.plumly@gmail.com>
2024-11-22 07:57:02 -0800

    modal test

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+
+const delay = 1000;
+
+document.getElementById('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    setTimeout(() => {
+        document.getElementById('thank-you-modal').style.display = 'block';
+    }, delay);
+})
+
 let thankYou = addEventListener('click', () => {
     console.log('message sent!')
 });
